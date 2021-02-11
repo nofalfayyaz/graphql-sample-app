c5e21d193fae7c8d3540175164e3652b6c1c57b0
fayyaznofal <fayyaznofal@gmail.com>
2021-02-11 16:52:56 +0500

    nested object support added

--- resolvers.js
+++ resolvers.js
@@ -18,9 +18,12 @@ const Query = {
 //for each single student object returned,resolver is invoked
 
 const Student = {
-   fullName:(root,args,context,info) => {
-      return root.firstName+" "+root.lastName
-   }
-}
+    fullName:(root,args,context,info) => {
+       return root.firstName+":"+root.lastName
+    },
+    college:(root) => {
+       return db.colleges.get(root.collegeId);
+    }
+ }
 
 module.exports = {Query,Student}
