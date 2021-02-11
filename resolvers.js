99bd230902b6ff0d75ff875e5d423d9d99f38e2c
fayyaznofal <fayyaznofal@gmail.com>
2021-02-11 16:43:58 +0500

    custom field support added

--- resolvers.js
+++ resolvers.js
@@ -1,13 +1,26 @@
 const db = require('./db')
 const Query = {
+   //resolver function for greeting
    greeting:() => {
-      return "hello from  Nofal !!!"
+      return "hello from  TutorialsPoint !!!"
    },
-   students:() => db.students.list(),
    
+   //resolver function for students returns list
+   students:() => db.students.list(),
+
+   //resolver function for studentbyId
    studentById:(root,args,context,info) => {
-    return db.students.get(args.id);
- }
+      //args will contain parameter passed in query
+      return db.students.get(args.id);
+   }
 }
 
-module.exports = {Query}
+//for each single student object returned,resolver is invoked
+
+const Student = {
+   fullName:(root,args,context,info) => {
+      return root.firstName+" "+root.lastName
+   }
+}
+
+module.exports = {Query,Student}
