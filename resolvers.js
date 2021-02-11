9187be708fedda5abf3afea6fa93a0edb7d79bdf
fayyaznofal <fayyaznofal@gmail.com>
2021-02-11 16:37:54 +0500

    get by id support dded

--- resolvers.js
+++ resolvers.js
@@ -3,7 +3,11 @@ const Query = {
    greeting:() => {
       return "hello from  Nofal !!!"
    },
-   students:() => db.students.list()
+   students:() => db.students.list(),
+   
+   studentById:(root,args,context,info) => {
+    return db.students.get(args.id);
+ }
 }
 
 module.exports = {Query}
